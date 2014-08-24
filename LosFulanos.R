10ec27cdc62e78adf40923dc10263d4327dd89bb
juliosergio <juliosergio@gmail.com>
2014-08-24 00:14:14 -0500

    Se agregó gráficos de pastel al cap. 6

--- LosFulanos.R
+++ LosFulanos.R
@@ -10,3 +10,10 @@ transporte <- c(
     "maritimo", "terrestre", "aereo",
     "terrestre", "terrestre", "aereo")
 info <- data.frame(participantes, transporte)
+plot(info$transporte,
+     las=2) # hace los textos perpendiculares al eje
+tt <- table(info$transporte)
+pie(tt, col=terrain.colors(4))
+rr <-tt/sum(tt)
+barplot(rr,col=rainbow(4))
+pie(tt, col=rainbow(4))
