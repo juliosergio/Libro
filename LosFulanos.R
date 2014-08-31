317bb4e5a325cef25bc383507d4e65d49aa96a81
juliosergio <juliosergio@gmail.com>
2014-08-30 22:20:58 -0500

    Se sigue trabajando en cap. 6: graficación

--- LosFulanos.R
+++ LosFulanos.R
@@ -17,3 +17,12 @@ pie(tt, col=terrain.colors(4))
 rr <-tt/sum(tt)
 barplot(rr,col=rainbow(4))
 pie(tt, col=rainbow(4))
+
+congreso2014 <- c(aereo=0.12, maritimo=0.1875, terrestre=0.4925, tren=0.2)
+rr1 <- cbind(rr, congreso2014)
+colnames(rr1) <- c("2012","2014")
+rr1
+
+barplot(t(rr1), beside=T, col=c(1,2))
+legend("topleft", legend=c("2012","2014"), col=c(1,2), pch=15)
+
