11193d19ccb4572ae073318eb1c22a63bd7b8981
juliosergio <juliosergio@gmail.com>
2014-09-09 10:09:58 -0500

    Se agrega ejemplo de dado de 6 caras a cap. 6

--- discretas.R
+++ discretas.R
@@ -1,2 +1,11 @@
-p <- rep(1/6, 6)
-pp <- Reduce("+", p, accumulate=T)
+ff <- rep(1/6, 6)
+ff
+FF <- Reduce("+", ff, accumulate=T)
+FF
+
+plot(ff, type="h", col="red", lwd=2, xlab="y", ylab="f(y)")
+points(ff)
+
+plot(FF, type="s", col="red", lwd=2, xlab="y", ylab="F(y)")
+points(FF, pch=16)
+
