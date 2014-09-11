5fa0a19a3d83da556e488a167d45ffd9337f6f30
juliosergio <juliosergio@gmail.com>
2014-09-10 20:39:26 -0500

    trabajando sobre distribucion de Poisson

--- EjemploPoissons.R
+++ EjemploPoissons.R
@@ -1,11 +1,22 @@
 ## Poisson distributions
 
-x <- 1:20
+x <- 0:20
+ds10 <- dpois(x, 10)
+plot(x=x, y=ds10, type="h", lwd=2, xlab="y", ylab="ds10(y)")
+# Por claridad se agregan puntos al gráfico:
+points(x, ds10, pch=21, bg="red")
+
+Ps10 <- Reduce('+', ds10, accumulate=T)
+plot(x, Ps10, type="s", col="red", lwd=2, xlab="y", ylab="Ps10(y)") 
+points(x, Ps10, pch=16)
+
+
 cc <- c("yellow", "springgreen", "navyblue")
 lambdas <- c(6, 10, 13)
 Ps <- as.data.frame(lapply(lambdas, function(ll) ppois(x, ll)))
 ds <- as.data.frame(lapply(lambdas, function(ll) dpois(x, ll)))
 names(Ps) <- lambdas
+names(ds) <- lambdas
 funcs <- list(plot, lines)
 
 for (i in 1:3) {
