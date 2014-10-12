82b1cd8746a7acd468e2ca251a185d45128bffb0
juliosergio <juliosergio@gmail.com>
2014-10-11 19:30:25 -0500

    Corrección de Fig. en cap 5

--- Gammas.R
+++ Gammas.R
@@ -7,11 +7,16 @@ d <- data.frame(
     stringsAsFactors=F
 )
 
-txt <- paste0("k=", d$k, ", theta=", d$theta)
-
+# txt <- paste0("k=", d$k, ", theta=", d$theta)
+txt <- as.expression(
+    lapply(1:nrow(d),
+           function(i) bquote(f(y,k==.(d[i,1]), theta==.(d[i,2])))
+    )
+)
+    
 ff <- function(x) dgamma(x, shape=d[1,]$k, scale=d[1,]$theta)
 curve(ff, main="", col=d[1,]$col, xlim=c(0,20), ylim=c(0, 0.5),
-      xlab="y", ylab="p")
+      xlab="y", ylab=expression(f(y,k,theta)))
 
 for (i in 2:nrow(d)) {
   ff <- function(x) dgamma(x, shape=d[i,]$k, scale=d[i,]$theta)
