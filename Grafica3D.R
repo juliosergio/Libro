93e0da5503e99c43530d1dc9c4369d03e90f2dee
juliosergio <juliosergio@gmail.com>
2014-10-29 12:36:53 -0600

    trabajo sobre lo de 3D

--- Grafica3D.R
+++ Grafica3D.R
@@ -1,15 +1,25 @@
 # Ejamplo de graficacion 3D
-g <- function(x) x/2
-h <- function(y) (1+3*y^2)/2
+# g <- function(x) x/2
+g <- function(x) x
+# h <- function(y) (1+3*y^2)/2
+h <- function(y) y
 f <- function(x,y) g(x)*h(y)
-x <- seq(0,2, length.out=16)
-y <- seq(0,1, length.out=8)
+x <- seq(-1,1, length.out=16)
+y <- seq(-1,1, length.out=16)
 z <- outer(x,y,f)
 
+titulo="Paraboloide hiperbólico"
+
 x11()
-
-persp(x,y,zz,theta = 30, phi = 30, expand=0.7, col = "lightblue",
-      ltheta = 120, shade = 0.75, ticktype = "detailed",
-      xlab = "X", ylab = "Y", zlab = "zz", main="PRUEBA"
-) -> res # Se guarda la transformacion
-
+theta=30
+phi=10
+repeat {
+    ss <- readline("SU-VAR>")
+    if (nchar(ss)==0) break
+    aa <- strsplit(ss, "=")
+    assign(aa[[1]][1],as.numeric(aa[[1]][2]))
+    persp(x,y,z,theta = theta, phi = phi, expand=0.7, col = "lightblue",
+        ltheta = 120, shade = 0.75, ticktype = "detailed",
+        xlab = "X", ylab = "Y", zlab = "z", main=titulo
+    ) -> res # Se guarda la transformacion
+}
